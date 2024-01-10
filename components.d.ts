8928f51cf932e7b6e26738140edd0a66cb3351f7
晚风 <737616626@qq.com>
2024-01-10 21:54:47 +0800

    feature: 添加面包屑

--- components.d.ts
+++ components.d.ts
@@ -5,33 +5,28 @@
 // Read more: https://github.com/vuejs/core/pull/3399
 export {}
 
-declare module 'vue' {
-  export interface GlobalComponents {
-    ElAvatar: typeof import('element-plus/es')['ElAvatar']
-    ElButton: typeof import('element-plus/es')['ElButton']
-    ElCheckbox: typeof import('element-plus/es')['ElCheckbox']
-    ElCol: typeof import('element-plus/es')['ElCol']
-    ElDivider: typeof import('element-plus/es')['ElDivider']
-    ElDrawer: typeof import('element-plus/es')['ElDrawer']
-    ElForm: typeof import('element-plus/es')['ElForm']
-    ElFormItem: typeof import('element-plus/es')['ElFormItem']
-    ElIcon: typeof import('element-plus/es')['ElIcon']
-    ElInput: typeof import('element-plus/es')['ElInput']
-    ElLink: typeof import('element-plus/es')['ElLink']
-    ElMenu: typeof import('element-plus/es')['ElMenu']
-    ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
-    ElPopover: typeof import('element-plus/es')['ElPopover']
-    ElRow: typeof import('element-plus/es')['ElRow']
-    ElScrollbar: typeof import('element-plus/es')['ElScrollbar']
-    ElSubMenu: typeof import('element-plus/es')['ElSubMenu']
-    ElSwitch: typeof import('element-plus/es')['ElSwitch']
-    ElTable: typeof import('element-plus/es')['ElTable']
-    ElTableColumn: typeof import('element-plus/es')['ElTableColumn']
-    ElTabPane: typeof import('element-plus/es')['ElTabPane']
-    ElTabs: typeof import('element-plus/es')['ElTabs']
-    HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
-    RouterLink: typeof import('vue-router')['RouterLink']
-    RouterView: typeof import('vue-router')['RouterView']
-    SvgIcon: typeof import('./src/components/SvgIcon/index.vue')['default']
-  }
+declare module "vue" {
+	export interface GlobalComponents {
+		ElAvatar: typeof import("element-plus/es")["ElAvatar"];
+		ElBadge: typeof import("element-plus/es")["ElBadge"];
+		ElBreadcrumb: typeof import("element-plus/es")["ElBreadcrumb"];
+		ElBreadcrumbItem: typeof import("element-plus/es")["ElBreadcrumbItem"];
+		ElDrawer: typeof import("element-plus/es")["ElDrawer"];
+		ElDropdown: typeof import("element-plus/es")["ElDropdown"];
+		ElDropdownItem: typeof import("element-plus/es")["ElDropdownItem"];
+		ElDropdownMenu: typeof import("element-plus/es")["ElDropdownMenu"];
+		ElIcon: typeof import("element-plus/es")["ElIcon"];
+		ElMenu: typeof import("element-plus/es")["ElMenu"];
+		ElMenuItem: typeof import("element-plus/es")["ElMenuItem"];
+		ElPopover: typeof import("element-plus/es")["ElPopover"];
+		ElScrollbar: typeof import("element-plus/es")["ElScrollbar"];
+		ElSubMenu: typeof import("element-plus/es")["ElSubMenu"];
+		ElSwitch: typeof import("element-plus/es")["ElSwitch"];
+		ElTabPane: typeof import("element-plus/es")["ElTabPane"];
+		ElTabs: typeof import("element-plus/es")["ElTabs"];
+		HelloWorld: typeof import("./src/components/HelloWorld.vue")["default"];
+		RouterLink: typeof import("vue-router")["RouterLink"];
+		RouterView: typeof import("vue-router")["RouterView"];
+		SvgIcon: typeof import("./src/components/SvgIcon/index.vue")["default"];
+	}
 }
